c905cb6157035316f5b9a1f774b38e0155de4179
Sandeep Singh <SandeepS@talentica-all.com>
2020-01-08 12:59:46 +0530

    BST tree rendering on the index _number

--- Express/routes/index.js
+++ Express/routes/index.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var path = require('path');
+var http = require('http');
+var orgnization = require('../module/orgnization.js');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log("here")
+  http.get('http://172.19.101.133:3000/get_data_in_preorder_form', (resp) => {
+    let data = '';
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+    resp.on('end', () => {
+       //console.log(data)
+       var orgstucter = orgnization.getOrnizationTree(JSON.parse(data));
+       res.send(orgstucter);
+    });
+  }).on("error", (err) => {
+    res.status(404).json("Error: " + err.message);
+  }).end();
+
+  next();
 });
 
 router.get('/Error', (req,res,next)=>{
